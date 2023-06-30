28bad4271d614da6e367360e5a565e992bddd18c
ItsAndhika <bambangindaryanto12@gmail.com>
2023-07-01 06:35:47 +0700

    Fix bug on navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     }, [isActive])
   return (
     <>
-    <header className='w-full fixed left-0 top-0 py-5 px-[16px] z-10 bg-transparent backdrop-blur-md md:py-10'>
+    <header className='fixed left-0 right-0 py-5 z-10 bg-transparent backdrop-blur-md md:py-10'>
         <div className="container flex justify-between items-center sm:px-5 xl:px-16">
             <a href="#" className="text-xl font-bold font-montserrat text-primary">PA</a>
             <nav>
@@ -28,9 +28,12 @@ const Header = () => {
                     <span className='bg-white w-1/2 h-[2px] origin-right transition duration-300 ease-in-out group-hover:bg-primary'></span>
                 </div>
                 <div className={`${isActive ? '-translate-x-0' : 'translate-x-[700px]'} fixed top-0 right-0 flex flex-col w-3/4 h-screen bg-black z-40 transition duration-500 ease-in-out p-10 md:hidden`}>
-                    <div className='group w-6 h-4 flex flex-col justify-between items-center cursor-pointer self-end mb-10 md:hidden' onClick={() => setIsActive(prev => !prev)}>
-                        <span className='w-full h-[2px] bg-white transition duration-300 ease-in-out origin-left -translate-y-0.5 rotate-45 group-hover:bg-primary'></span>
-                        <span className='w-full h-[2px] bg-white transition duration-300 ease-in-out origin-left -rotate-45 group-hover:bg-primary'></span>
+                    <div className="flex justify-between w-full px-5">
+                        <h2 className='font-semibold text-lg font-montserrat self-start'>Menus</h2>
+                        <div className='group w-6 h-4 flex flex-col justify-between items-center cursor-pointer self-end mb-10 md:hidden' onClick={() => setIsActive(prev => !prev)}>
+                            <span className='w-full h-[2px] bg-white transition duration-300 ease-in-out origin-left -translate-y-0.5 rotate-45 group-hover:bg-primary'></span>
+                            <span className='w-full h-[2px] bg-white transition duration-300 ease-in-out origin-left -rotate-45 group-hover:bg-primary'></span>
+                        </div>
                     </div>
                     <ul className='h-3/4 flex flex-col justify-around items-center mx-auto'>
                         <li className='text-xl transition duration-500 ease-in-out hover:text-primary hover:text-gold-glow' onClick={() => setIsActive(prev => !prev)}><a href="#">Home</a></li>
@@ -39,11 +42,10 @@ const Header = () => {
                         <li className='text-xl transition duration-500 ease-in-out hover:text-primary hover:text-gold-glow' onClick={() => setIsActive(prev => !prev)}><a href="#contact">Contact</a></li>
                     </ul>
                 </div>
-                <div className={`${isActive ? 'block' : 'hidden'} top-0 left-0 z-20 fixed w-full h-screen bg-transparent`} onClick={() => setIsActive(prev => !prev)}></div>
+                <div className={`${isActive ? 'block' : 'hidden'} backdrop-blur-sm top-0 left-0 z-20 fixed w-full h-screen bg-transparent transition duration-500 ease-in-out`} onClick={() => setIsActive(prev => !prev)}></div>
             </nav>
         </div>
     </header>
-    
     </>
   )
 }
