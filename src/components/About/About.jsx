6e19d5e363b45da0380956283efaa7fe075c5a3d
ItsAndhika <bambangindaryanto12@gmail.com>
2023-07-03 06:06:54 +0700

    Fix my bad english grammar

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,58 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <section id="about" className='pt-36'>
-        <h1 className='text-3xl text-center font-bold text-primary 2xl:text-4xl' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>About Me</h1>
-        <div className="container flex flex-col items-center px-10 sm:px-5 xl:px-16">
-            <article className='flex flex-col justify-center items-center mx-auto pt-10 md:flex-row'>
-                <img src="/assets/images/profile.webp" alt="Profile" className='rounded-2xl w-full shadow-gold-glow animate-shadow-pulse sm:w-96 md:w-1/3' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'/>
-              <div className="w-full flex flex-col md:w-2/3 md:pl-20 lg:justify-between">
-                <p className='text-lg mt-10 font-open-sans max-w-sm md:mt-0 lg:max-w-md lg:text-xl lg:mb-10 xl:max-w-full xl:text-xl 2xl:text-2xl' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>Hi there!. My name is Primananda Andhika, i am from Central Java, Indonesia. Currently i live in Purworejo with my family. I've learned programming since i was at class 8 Junior High School until now i'm 15 years old.</p>
-                <p className='text-lg mt-10 font-open-sans max-w-sm md:mt-5 lg:max-w-md lg:text-xl lg:mt-10 xl:max-w-full xl:text-xl 2xl:text-2xl' data-aos="fade-up" data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>First programming language that i learned is JavaScript and i like that language so much. I'm also learning foreign languages like English, Spanish, French, Chinese but i'm still not fluent with those languages yet.</p>
-              </div>
-            </article>
-        </div>
-    </section>
-  )
-}
+	return (
+		<section id="about" className="pt-36">
+			<h1
+				className="text-3xl text-center font-bold text-primary 2xl:text-4xl"
+				data-aos="fade-up"
+				data-aos-easing="ease-in-out"
+				data-aos-once="true"
+				data-aos-duration="1000"
+			>
+				About Me
+			</h1>
+			<div className="container flex flex-col items-center px-10 sm:px-5 xl:px-16">
+				<article className="flex flex-col justify-center items-center mx-auto pt-10 md:flex-row">
+					<img
+						src="/assets/images/profile.webp"
+						alt="Profile"
+						className="rounded-2xl w-full shadow-gold-glow animate-shadow-pulse sm:w-96 md:w-1/3"
+						data-aos="fade-up"
+						data-aos-easing="ease-in-out"
+						data-aos-once="true"
+						data-aos-duration="1000"
+					/>
+					<div className="w-full flex flex-col md:w-2/3 md:pl-20 lg:justify-between">
+						<p
+							className="text-lg mt-10 font-open-sans max-w-sm md:mt-0 lg:max-w-md lg:text-xl lg:mb-10 xl:max-w-full xl:text-xl 2xl:text-2xl"
+							data-aos="fade-up"
+							data-aos-easing="ease-in-out"
+							data-aos-once="true"
+							data-aos-duration="1000"
+						>
+							Hi there! My name is Primananda Andhika, i am from Central Java,
+							Indonesia. Currently i live in Purworejo with my family. I've been
+							programming since I was at class 8 Junior High School, and now i'm
+							15 years old.
+						</p>
+						<p
+							className="text-lg mt-10 font-open-sans max-w-sm md:mt-5 lg:max-w-md lg:text-xl lg:mt-10 xl:max-w-full xl:text-xl 2xl:text-2xl"
+							data-aos="fade-up"
+							data-aos-easing="ease-in-out"
+							data-aos-once="true"
+							data-aos-duration="1000"
+						>
+							The first programming language that I learned is JavaScript and i
+							love that language so much. I'm also learning foreign languages
+							like English, Spanish, French, Chinese but i'm still not fluent
+							with those languages yet.
+						</p>
+					</div>
+				</article>
+			</div>
+		</section>
+	);
+};
 
-export default About
+export default About;
--- src/components/Skills/parts/FrontEnd.jsx
+++ src/components/Skills/parts/FrontEnd.jsx
@@ -1,45 +1,89 @@
-import HTML from '/assets/svg/html.svg'
-import CSS from '/assets/svg/css.svg'
-import JavaScript from '/assets/svg/javascript.svg'
-import Bootstrap from '/assets/svg/bootstrap.svg'
-import Tailwind from '/assets/svg/tailwind.svg'
-import Reactjs from '/assets/svg/react.svg'
+import HTML from "/assets/svg/html.svg";
+import CSS from "/assets/svg/css.svg";
+import JavaScript from "/assets/svg/javascript.svg";
+import Bootstrap from "/assets/svg/bootstrap.svg";
+import Tailwind from "/assets/svg/tailwind.svg";
+import Reactjs from "/assets/svg/react.svg";
 
 const FrontEnd = () => {
-  return (
-    <>
-        <div className="skill-card" data-aos="fade-up"  data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-            <img src={HTML} alt="HTML" className='w-14 mx-auto 2xl:w-20' />
-            <p className='text-lg font-semibold my-1'>HTML</p>
-            <p className='text-base font-medium'>Advance</p>
-        </div>
-        <div className="skill-card" data-aos="fade-up"  data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-            <img src={CSS} alt="CSS" className='w-14 mx-auto 2xl:w-20' />
-            <p className='text-lg font-semibold my-1'>CSS</p>
-            <p className='text-base font-medium'>Advance</p>
-        </div>
-        <div className="skill-card" data-aos="fade-up"  data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-            <img src={JavaScript} alt="JavaScript" className='w-10 mx-auto 2xl:w-16' />
-            <p className='text-lg font-semibold my-1'>JavaScript</p>
-            <p className='text-base font-medium'>Intermediate</p>
-        </div>
-        <div className="skill-card" data-aos="fade-up"  data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-            <img src={Bootstrap} alt="Bootstrap" className='w-10 mx-auto 2xl:w-16' />
-            <p className='text-lg font-semibold my-1'>Bootstrap</p>
-            <p className='text-base font-medium'>Intermediate</p>
-        </div>
-        <div className="skill-card" data-aos="fade-up"  data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-            <img src={Tailwind} alt="Tailwind" className='w-10 mx-auto 2xl:w-16' />
-            <p className='text-lg font-semibold my-1'>Tailwind</p>
-            <p className='text-base font-medium'>Intermediate</p>
-        </div>
-        <div className="skill-card" data-aos="fade-up"  data-aos-easing='ease-in-out' data-aos-once='true' data-aos-duration='1000'>
-            <img src={Reactjs} alt="React.js" className='w-10 mx-auto 2xl:w-16' />
-            <p className='text-lg font-semibold my-1'>React.js</p>
-            <p className='text-base font-medium'>Beginner</p>
-        </div>
-    </>
-  )
-}
+	return (
+		<>
+			<div
+				className="skill-card"
+				data-aos="fade-up"
+				data-aos-easing="ease-in-out"
+				data-aos-once="true"
+				data-aos-duration="1000"
+			>
+				<img src={HTML} alt="HTML" className="w-14 mx-auto 2xl:w-20" />
+				<p className="text-lg font-semibold my-1">HTML</p>
+				<p className="text-base font-medium">Advanced</p>
+			</div>
+			<div
+				className="skill-card"
+				data-aos="fade-up"
+				data-aos-easing="ease-in-out"
+				data-aos-once="true"
+				data-aos-duration="1000"
+			>
+				<img src={CSS} alt="CSS" className="w-14 mx-auto 2xl:w-20" />
+				<p className="text-lg font-semibold my-1">CSS</p>
+				<p className="text-base font-medium">Advanced</p>
+			</div>
+			<div
+				className="skill-card"
+				data-aos="fade-up"
+				data-aos-easing="ease-in-out"
+				data-aos-once="true"
+				data-aos-duration="1000"
+			>
+				<img
+					src={JavaScript}
+					alt="JavaScript"
+					className="w-10 mx-auto 2xl:w-16"
+				/>
+				<p className="text-lg font-semibold my-1">JavaScript</p>
+				<p className="text-base font-medium">Intermediate</p>
+			</div>
+			<div
+				className="skill-card"
+				data-aos="fade-up"
+				data-aos-easing="ease-in-out"
+				data-aos-once="true"
+				data-aos-duration="1000"
+			>
+				<img
+					src={Bootstrap}
+					alt="Bootstrap"
+					className="w-10 mx-auto 2xl:w-16"
+				/>
+				<p className="text-lg font-semibold my-1">Bootstrap</p>
+				<p className="text-base font-medium">Intermediate</p>
+			</div>
+			<div
+				className="skill-card"
+				data-aos="fade-up"
+				data-aos-easing="ease-in-out"
+				data-aos-once="true"
+				data-aos-duration="1000"
+			>
+				<img src={Tailwind} alt="Tailwind" className="w-10 mx-auto 2xl:w-16" />
+				<p className="text-lg font-semibold my-1">Tailwind</p>
+				<p className="text-base font-medium">Intermediate</p>
+			</div>
+			<div
+				className="skill-card"
+				data-aos="fade-up"
+				data-aos-easing="ease-in-out"
+				data-aos-once="true"
+				data-aos-duration="1000"
+			>
+				<img src={Reactjs} alt="React.js" className="w-10 mx-auto 2xl:w-16" />
+				<p className="text-lg font-semibold my-1">React.js</p>
+				<p className="text-base font-medium">Beginner</p>
+			</div>
+		</>
+	);
+};
 
-export default FrontEnd
+export default FrontEnd;
